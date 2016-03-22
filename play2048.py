4b927518ce30aa736f5a93a09be430eb3629b8c4
SeanAbbott1 <sabbott2012@gmail.com>
2016-03-21 22:15:56 -0400

    Sean A Wk 11 Sub 1

--- play2048.py
+++ play2048.py
@@ -1,10 +1,34 @@
 #TODO: Import the module that will allow you to use Selenium
+
+from selenium import webdriver
+
 #TODO: Import the module that will allow you to use the up, down, left, and right keys on your keyboard
+
+from selenium.webdriver.common.keys import Keys
 
 def play2048( times ):
     #TODO: write code in this function that:
     # 1. opens a game of 2048 from the URL: https://gabrielecirulli.github.io/2048/
+
+    browser = webdriver.Firefox()
+    # type(browser)
+    browser.get('https://gabrielecirulli.github.io/2048/')
+
     # 2. uses the parameter 'times' to determine how many times your code will try to play the game
+
+    htmltagElem = browser.find_element_by_tag_name('html')
+    FinScoreElem = browser.find_element_by_class_name('score-container')
+       
     # 3. for each 'time', press these keys in this order: UP, RIGHT, DOWN, LEFT
-    # 4. print the final score after all tries to the screen 
+
+    turn = 0 
+    for turn in range(times):
+        htmltagElem.send_keys(Keys.UP)
+        htmltagElem.send_keys(Keys.RIGHT)
+        htmltagElem.send_keys(Keys.DOWN)
+        htmltagElem.send_keys(Keys.LEFT)  
+        turn += 1
     
+    # 4. print the final score after all tries to the screen
+
+    print('Congratulations !! Your Final score = '+FinScoreElem.text)
